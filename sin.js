f222d234b84a39464051a5054def282a5465d68c
Arian van Putten <aeroboy94@gmail.com>
2012-09-19 23:13:44 +0200

    fixed stuff

--- sin.js
+++ sin.js
@@ -1,12 +1,12 @@
 var unamplified  = document.getElementById("unamplified").getContext("2d");
 
-var amplified  = document.getElementById("amplified").getContext("2d");
 
 
 function Graph(ctx) {
     this.ctx = ctx;
     this.height = ctx.canvas.height;
     this.width = ctx.canvas.width;
+    ctx.font = "16px monospace";
 }
 
 var TAU = 2 * Math.PI;
@@ -16,15 +16,16 @@ var yMin = -2;
 var yMax = 2;
 
 Graph.prototype.XC = function XC(x) {
-    return x / xMax * this.width; 
-}
+    return x / xMax * this.width;
+};
 
 Graph.prototype.YC = function YC(y) {
     return this.height - (y - yMin) / (yMax - yMin) * this. height;
-}
+};
 
 Graph.prototype.drawAxes = function drawAxes() {
-    this.ctx.strokeStyle = "white";
+    this.ctx.strokeStyle = "rgba(255,255,255,0.5)";
+    this.ctx.lineWidth = 2;
 
     this.ctx.beginPath();
     this.ctx.moveTo(this.XC(0), this.YC(0));
@@ -36,9 +37,13 @@ Graph.prototype.drawAxes = function drawAxes() {
     this.ctx.lineTo(this.XC(xMin), this.YC(0));
     this.ctx.stroke();
 
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.XC(xMax/2), this.YC(yMax));
+    this.ctx.lineTo(this.XC(xMax/2), this.YC(yMin));
+    this.ctx.stroke();
 
 
-}
+};
 
 Graph.prototype.render = function render( fs, colors, pretty) {
     var colorPos = 0;
@@ -53,7 +58,9 @@ Graph.prototype.render = function render( fs, colors, pretty) {
 
             self.ctx.strokeStyle = self.ctx.fillStyle = colors[colorPos];
             if (first) {
-                self.ctx.fillText(pretty[prettyPos] + " => " + y.toFixed(2), 0, self.XC(textPos)); 
+                self.ctx.strokeStyle = self.ctx.fillStyle = colors[colorPos].replace("0.4", "0.9");
+                self.ctx.fillText(pretty[prettyPos] + " => " + y.toFixed(2), 0, self.XC(textPos));
+                self.ctx.fillStyle = colors[colorPos];
                 self.ctx.moveTo(self.XC(x), self.YC(y));
                 first = false;
             } else {
@@ -63,51 +70,47 @@ Graph.prototype.render = function render( fs, colors, pretty) {
         }
         
         self.ctx.stroke();
-        textPos += 1;
+        textPos += 0.8;
         colorPos++;
         prettyPos++;
-    }); 
-}
+    });
+};
 
 Graph.prototype.clearScreen = function clearScreen() {
     this.ctx.fillStyle = 'black';
     this.ctx.fillRect(0,0, this.width, this.height);
-}; 
+};
+
 var F = Math.sin;
 var G = function(x) { return  Math.sin( x + TAU / 3); };
-var H = function(x) { return  Math.sin(x + (2*TAU) / 3); };
+var H = function(x) { return  Math.sin( x + (2*TAU) / 3); };
 
 var c = 0;
 
 var unamplifiedGraph = new Graph(unamplified);
-var amplifiedGraph = new Graph(amplified);
 setInterval(function() {
     unamplifiedGraph.clearScreen();
     unamplifiedGraph.render([
-         (function(x){return F(x-c);}),
-         (function(x){return G(x-c);}),
-         (function(x){return H(x-c);})
-    ], [
-        "ff0000",
-        "00ff00",
-        "0000ff"
-    ],
-    [
-        "sin x",
-        "sin (x + 2pi/3)",
-        "sin (x + 4pi/3)"
-    ]);
-    unamplifiedGraph.drawAxes();
-
-    c +=  xMax / unamplifiedGraph.width;
-    
-    amplifiedGraph.clearScreen();
-    amplifiedGraph.render([
+        (function(x){return F(x-c);}),
+        (function(x){return G(x-c);}),
+        (function(x){return H(x-c);}),
         (function(x) {return F(x-c) > G(x-c) ? 1 : 0;}),
         (function(x) {return F(x-c) > H(x-c) ? 1 : 0;})
+    ], [
+        "rgba(255,0,0,0.4)",
+        "rgba(0,255,0,0.4)",
+        "rgba(255,100,0,0.4)",
+        "ffff00",
+        "ff00ff"
     ],
-    ["#ffff00", "#ff00ff"],
-    ["sin x > sin (x + 2pi/3)", "sin x > sin (x + 4pi/3)"]
-    );
-    amplifiedGraph.drawAxes();
-}, 40);
+    [
+        "f(x) = sin x",
+        "g(x) = f(x + 2pi/3)",
+        "h(x) = g(x + 2pi/3)",
+        "f(x) > g(x)",
+        "f(x) > h(x)"
+    ]);
+    
+    unamplifiedGraph.drawAxes();
+    c +=  (xMax / unamplifiedGraph.width);
+}, 20);
